602e39a40bd9df5034652b51828c976aea1e590c
Joey Zheng <jzheng@comply.com>
2024-03-27 20:34:40 -0400

    Refactor port to use env variable

--- ex2.ts
+++ ex2.ts
@@ -12,7 +12,7 @@ import { Sequelize, DataTypes } from "sequelize";
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
